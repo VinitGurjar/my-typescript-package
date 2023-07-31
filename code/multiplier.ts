3a353da5ea2fa75271f42d8b60a77837fd2afdba
adhiramaa <vinitfagna@gmail.com>
2023-08-01 00:13:04 +0530

    Add document level comment for the custom type

--- code/multiplier.ts
+++ code/multiplier.ts
@@ -4,7 +4,22 @@
  * }
  * multiplier(4, 8, 'when 4 and 8 fussed they make ::><:');
  */
+
 //Creating my own type (string literal union type)
+
+/**
+ * In JavaScript, the code would require additional validation
+ *  to make sure the last argument is indeed a string. TypeScript 
+ * offers a way to define specific types for inputs, which describe 
+ * exactly what type of input is acceptable. On top of that, TypeScript
+ *  can also show the info on the accepted values already at the editor level.
+
+ * We can create a type using the TypeScript native keyword type.
+ * Let's describe our type Operation:
+
+ * type Operation = 'multiply' | 'add' | 'divide';
+ */
+
 type Operation = 'add' | 'multiply' | 'divide';
 
 const calculator = (a: number, b: number, op: Operation) => {
