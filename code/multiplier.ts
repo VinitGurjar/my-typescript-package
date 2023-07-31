e104604a222b0218410349f3a05ab4347b13c9d2
adhiramaa <vinitfagna@gmail.com>
2023-08-01 00:00:37 +0530

    Create custom type

--- code/multiplier.ts
+++ code/multiplier.ts
@@ -1,5 +1,21 @@
-const multiplier = (a: number, b: number, printText: string) => {
-    console.log(printText, a * b);
+/**
+ * const multiplier = (a: number, b: number, printText: string)  => {
+ *  console.log(printText, a * b);
+ * }
+ * multiplier(4, 8, 'when 4 and 8 fussed they make ::><:');
+ */
+//Creating my own type (string literal union type)
+type Operation = 'add' | 'multiply' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation) => {
+    if (op === 'add') {
+        return a + b;
+    } else if (op === 'multiply') {
+        return a * b;
+    } else if (op === 'divide') {
+        if (b === 0) return 'can\'t divide by 0!';
+        return a / b;
+    }
 }
 
-multiplier(4, 8, 'when 4 and 8 fussed they make ::><:');
+calculator(5, 5, 'add');
